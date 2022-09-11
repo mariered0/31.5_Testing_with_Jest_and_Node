540e3a2cc1202286f1ac8a6d11adce4f6910a150
mariered0 <98713565+mariered0@users.noreply.github.com>
2022-09-10 18:59:55 -0700

    Remove console.log

--- markov.js
+++ markov.js
@@ -19,23 +19,20 @@ class MarkovMachine {
   makeChains() {
     // TODO
     //an object to put everything in
+
     const chain = {};
     const words = this.words;
     //putting the word into the object
     for (let i = 0; i < words.length; i++) {
       //check if the word is in the chain obj
       if (words[i] in chain) {
-        console.log('word in chain:', words[i]);
         //if in chain, check if the next word is in the value array. if not, add the word in arr
         if (!chain[words[i]].includes(words[i + 1])) {
-          console.log('word adding newly:', words[i + 1]);
           chain[words[i]].push(words[i + 1]);
         }
       }
       //if the word is not in chain obj
       else {
-        console.log('adding key in chain:', words[i])
-        console.log('chain:', chain);
         //if it's the last word, add an array with null
         if (words[i + 1] === undefined) {
           chain[words[i]] = [null];
@@ -45,7 +42,6 @@ class MarkovMachine {
         }
       }
     }
-    console.log('console', chain);
     return chain;
   }
 
@@ -61,15 +57,12 @@ class MarkovMachine {
     let previousWord;
     let i = 1;
     while (i <= numWords) {
-      console.log(`${i}th word`)
       //if this is NOT the first word
       if (previousWord) {
-        console.log('previous word:', previousWord);
         //look for the next word
         //generate random idx
         nextWordIdx = Math.floor(Math.random() * chain[previousWord].length);
         word = chain[previousWord][nextWordIdx];
-        console.log('next word:', word);
         //if the arr of the next word is null, end this
         if (chain[previousWord][nextWordIdx] === null) {
           i = numWords;
@@ -79,7 +72,6 @@ class MarkovMachine {
         //look for the word from keys
         const wordRandomIndex = Math.floor(Math.random() * Object.keys(chain).length);
         word = Object.keys(chain)[wordRandomIndex]
-        console.log('random key:', word);
       }
       //set that word to text
       if(word !== null){
@@ -88,15 +80,10 @@ class MarkovMachine {
       previousWord = word;
       i++;
     };
-    text.trim();
-    console.log('text:', text);
+    return text.trim();
 
   }
 }
-
-// let mm = new MarkovMachine("the cat in the hat");
-// mm.makeChains();
-// mm.makeText(numWords = 30);
 
 //export
 module.exports = {
